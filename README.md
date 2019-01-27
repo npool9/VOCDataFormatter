76d2902f1597a4b3afccf59ac1fa28147f5bdcd9
npool9 <npool@elon.edu>
2019-01-27 12:59:21 -0500

    Allow functionality for more than one object per image

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # VOC PASCAL Data Formatter
+
+This project takes any image dataset that you have and converts its format into VOC PASCAL format.
+This becomes useful when implementing image classification/object detection methods submitted to the VOC PASCAL competitions.
+Putting the image data in the format allows the development kit's evaluation functions to recognize the annotations of the images including their classes.
+
+#### Assumptions:
+1. Your own image dataset is separated into subdirectories corresponding to the class to which they belong.
+2. You have the VOCdevkit directory downloaded: http://host.robots.ox.ac.uk/pascal/VOC/voc2010/VOCdevkit_08-May-2010.tar
+    * You do not need the VOC PASCAL dataset provided you have your own dataset. You just need the development kit file system.
+3. There is one object per image.
+    * For classification purposes, the "bounding box" is just the border of the entire image.
+    * These assumptions can be edited in the annotatoin_maker.py script.
--- annotation_maker.py
+++ annotation_maker.py
@@ -26,17 +26,18 @@ class AnnotationMaker:
         self.year = year
         self.annotation_path = annotation_path
 
-    def xml_writer(self, filename, object_class, box, difficult='0', occluded='0', pose='Unspecified', truncated='0',
-                   segmented='0', database='Unknown', im='Unknown'):
+    def xml_writer(self, filename, object_class, box, num_objects=1, difficult='0', occluded='0', pose='Unspecified',
+                   truncated='0', segmented='0', database='Unknown', im='Unknown'):
         """
         The writer of the tags that populate each annotation xml file for each image.
         :param filename: the filename of the image (including its extension)
         :param object_class: the image/object's class (also the subdirectory name based on the script's assumptions)
-        :param box: the bounding box of the object i.e. np.array([xmax, xmin, ymax, ymin])
+        :param box: the bounding box of the objects in the image i.e. [[xmax, xmin, ymax, ymin], ...]
          ASSUMPTIONS:
           1. There is only one object per image
           2. The task is classification, and therefore the bounding box of each object is the border of the image
             NOTE: if you have other bounding box information, provide each bounding box as a row of a numpy array
+        :param num_objects: the number of objects in the image (ordered the same way as the box parameter)
         :param difficult: either 1 or 0
         :param occluded: either 1 or 0
         :param pose: not familiar with this parameter
@@ -47,14 +48,17 @@ class AnnotationMaker:
         :return: the contents of the xml annotation file for a single image
         """
         image = imread(self.dataset_path + '/' + object_class + '/' + filename)
+        object_tags = ""
+        for i in range(num_objects):
+            object_tags += ('\n\t<object>' + '\n\t\t<name>' + object_class.lower() +
+                  '</name>\n\t\t<bndbox>\n\t\t\t<xmax>' + box[i][0] + '</xmax>\n'
+                  + '\t\t\t<xmin>' + box[i][1] + '</xmin>\n\t\t\t<ymax>' + box[i][2] + '</ymax>\n\t\t\t' + '<ymin>' +
+                  box[i][3] + '</ymin>\n\t\t</bndbox>\n\t\t<difficult>' + difficult + '</difficult>\n\t\t<occluded>' +
+                  occluded + '</occluded>\n\t\t<pose>' + pose + '</pose>\n\t\t<truncated>' + truncated +
+                  '</truncated>\n\t</object>')
         xml = ('<annotation>\n\t<filename>' + filename + '</filename>\n\t<folder>VOC' + self.year +
-                  '</folder>\n\t<object>' +
-                  '\n\t\t<name>' + object_class.lower() + '</name>\n\t\t<bndbox>\n\t\t\t<xmax>' + box[0] + '</xmax>\n'
-                  + '\t\t\t<xmin>' + box[1] + '</xmin>\n\t\t\t<ymax>' + box[2] + '</ymax>\n\t\t\t' + '<ymin>' + box[3]
-                  + '</ymin>\n\t\t</bndbox>\n\t\t<difficult>' + difficult + '</difficult>\n\t\t<occluded>' + occluded +
-                  '</occluded>\n\t\t<pose>' + pose + '</pose>\n\t\t<truncated>' + truncated +
-                  '</truncated>\n\t</object>\n' + '<size>\n\t\t<depth>' + str(image.shape[2]) + '</depth>\n\t\t<height>' +
-                  str(image.shape[1]) + '</height>' + '\n\t\t<width>' + str(image.shape[0]) +
+                  '</folder>' + object_tags + '\n\t<size>\n\t\t<depth>' + str(image.shape[2]) + '</depth>\n\t\t<height>'
+                  + str(image.shape[1]) + '</height>' + '\n\t\t<width>' + str(image.shape[0]) +
                   '</width>\n\t</size>\n\t<segmented>' + segmented + '</segmented>' + '\n\t<source>\n\t\t' +
                   '<annotation>Nathan Pool</annotation>\n\t\t<database>' + database + '</database>'
                   + '\n\t\t<image>' + im + '</image>\n\t</source>\n</annotation>')
@@ -76,7 +80,9 @@ class AnnotationMaker:
                 for im_file in im_files:
                     image = imread(self.dataset_path + '/' + c + '/' + im_file)
                     # note our default bounding box implies classification vs detection (the border of the image)
-                    bbox = np.array([str(image.shape[0]), str(0), str(image.shape[1]), str(0)])
+                    #  you can provide your own list of lists structure to capture the number of objects in each
+                    #  of your images
+                    bbox = [[str(image.shape[0]), str(0), str(image.shape[1]), str(0)]]
                     xml = self.xml_writer(im_file, c, bbox)
                     # Now, save the file to the annotation path obeying VOC PASCAL formatting expectations
                     f = open(self.annotation_path + '/' + im_file[:im_file.index('.')] + '.xml', 'w')
